600011f780cf02275d1bed0155d970a6a2ed84c8
juan01jgt <106551797+juan01jgt@users.noreply.github.com>
2022-09-26 10:05:48 +0200

    Add files via upload

---
+++ Ejercicio4.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Juan Garcia
+ * Muestro por pantalla un radio y calculo y muestro su perimetro y su area, por último dibujo el circulo
+ */
+
+$ciclo = "DAW";
+$modulo = "DWES";
+
+print "<p>";
+printf("%s es un módulo de %d curso de %s",$modulo, 2, $ciclo);
+print "</p>";
+
+
+echo "</br>Código <a href='https://github.com/juan01jgt/DWES_ud2/blob/main/Ejercicio4.php'>aqui</a>";
