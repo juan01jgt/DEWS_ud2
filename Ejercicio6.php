b1b1aed418df4ca9a17f9a0cffe45fbf0be0d113
juan01jgt <106551797+juan01jgt@users.noreply.github.com>
2022-09-26 10:11:17 +0200

    Add files via upload

---
+++ Ejercicio6.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Juan Garcia
+ * Cargo las variables y muestro distintos resultados
+ */
+
+$x = 10;
+$y = 7;
+
+echo "$x + $y =",$num1+$num2;
+echo "$x - $y =",$num1-$num2;
+echo "$x * $y =",$num1*$num2;
+echo "$x / $y =",$num1/$num2;
+echo "$x % $y =",$num1%$num2;
+
+
+echo "</br>Código <a href='https://github.com/juan01jgt/DWES_ud2/blob/main/Ejercicio6.php'>aqui</a>";
