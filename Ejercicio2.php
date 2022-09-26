293564340301a7015387af32848bbcbd3ea281c6
juan01jgt <106551797+juan01jgt@users.noreply.github.com>
2022-09-26 09:40:45 +0200

    Add files via upload

--- Ejercicio2.php
+++ Ejercicio2.php
@@ -10,3 +10,6 @@
 echo "Nombre: ".$nombre;
 echo "</br> Apellidos: ".$apellidos;
 echo "</br> <img src='imagen.jpg' width='50%' height='50%'>";
+
+echo "</br>Código <a href='https://github.com/juan01jgt/DWES_ud2/blob/main/Ejercicio2.php'>aqui</a>";
+
