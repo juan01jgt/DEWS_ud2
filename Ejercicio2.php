dfc30abe280c949c187e0dc4a93162ac4cd6d00e
juan01jgt <106551797+juan01jgt@users.noreply.github.com>
2022-09-26 09:39:48 +0200

    Add files via upload

---
+++ Ejercicio2.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Juan Garcia
+ * Muestro por pantalla un Hola mundo y el enlace al código
+ */
+
+ $nombre = "Juan";
+ $apellidos = "Garcia Toril";
+
+echo "Nombre: ".$nombre;
+echo "</br> Apellidos: ".$apellidos;
+echo "</br> <img src='imagen.jpg' width='50%' height='50%'>";
