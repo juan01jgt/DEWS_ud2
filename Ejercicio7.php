da299e34ac12d1e58aeb15c25be9015debbb37d5
juan01jgt <106551797+juan01jgt@users.noreply.github.com>
2022-09-26 10:18:07 +0200

    Add files via upload

---
+++ Ejercicio7.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author Juan Garcia
+ * Cargo las variables y muestro distintos resultados
+ */
+
+$numero = 8;
+
+echo "Valor actual ",$numero;
+$numero = $numero+2;
+echo "</br>Suma 2. Valor ahora ",$numero;
+$numero = $numero-4;
+echo "</br>Resta 4. Valor ahora ",$numero;
+$numero = $numero*5;
+echo "</br>Multiplica por 5. Valor ahora ",$numero;
+$numero = $numero/3;
+echo "</br>Divide por 3. Valor ahora ",$numero;
+$numero++;
+echo "</br>Incrementa el valor en 1. Valor ahora ",$numero;
+$numero--;
+echo "</br>Decrementa el valor en 1. Valor ahora ",$numero;
+
+
+echo "</br>Código <a href='https://github.com/juan01jgt/DWES_ud2/blob/main/Ejercicio7.php'>aqui</a>";
