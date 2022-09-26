e6c56883be0eae54462914f40c1a5bf09f74a944
juan01jgt <106551797+juan01jgt@users.noreply.github.com>
2022-09-26 09:19:05 +0200

    Add files via upload

---
+++ Ejercicio1.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Juan Garcia
+ * Muestro por pantalla un Hola mundo y el enlace al código
+ */
+echo "Hola Mundo";
+
+echo "<br>Código <a href='https://github.com/juan01jgt/DWES_ud2.git'>aqui</a>";
+
+?>
